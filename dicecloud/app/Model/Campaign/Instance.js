cd668b7009f9ea7315d5c64d124819e283e931d0
kriath <42991716+kriath@users.noreply.github.com>
2018-09-13 15:54:39 +1000

    Removed dicecloud test

--- dicecloud/app/Model/Campaign/Instance.js
+++
@@ -1,7 +0,0 @@
-Instances = new Mongo.Collection("instances");
-
-Schemas.Instance = new SimpleSchema({
-	//an instance is a single flow of time all parties in an instance are in-sync time wise
-});
-
-Instances.attachSchema(Schemas.Instance);
--- dicecloud/app/server/publications/users.js
+++
@@ -1,7 +0,0 @@
-Meteor.publish("userNames", function(ids){
-	if (!this.userId || !ids) return [];
-	return Meteor.users.find(
-		{_id: {$in: ids}},
-		{fields: {username: 1}}
-	);
-});
